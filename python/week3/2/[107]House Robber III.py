ef6c622bce91ec7433772fb74ca572adb6f320cb
sxxgwoo <tjddn753@gmail.com>
2025-07-26 16:14:23 +0900

    7/26mac

--- python/week3/2/[107]House Robber III.py
+++ python/week3/2/[107]House Robber III.py
@@ -2,7 +2,8 @@
 House Robber III
 The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
 
-In this new place, there are houses and each house has its only one parent house. All houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken.
+In this new place, there are houses and each house has its only one parent house. All houses in this place form a binary tree. 
+It will automatically contact the police if two directly-linked houses were broken.
 
 You are given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
 
@@ -24,6 +25,94 @@ Input: root = [1,null,2,3,5,4,2]
 Output: 12
 Explanation: Maximum amount of money the thief can rob = 1 + 4 + 2 + 5 = 12
 '''
+class Queue:
+    def __init__(self):
+        self.data = []
+
+    def append(self, val):
+        self.data.append(val)
+
+    def popleft(self):
+        if self.data:
+            return self.data.pop(0)
+        raise IndexError("pop from empty queue")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __bool__(self):
+        return bool(self.data)
+
+# 입력 리스트를 TreeNode로 변환하는 유틸리티 함수
+def build_tree(nodes):
+    if not nodes or nodes[0] is None:
+        return None
+    root = TreeNode(nodes[0])
+    q = Queue()
+    q.append(root)
+    i = 1
+    while i < len(nodes):
+        node = q.popleft()
+        if i < len(nodes) and nodes[i] is not None:
+            node.left = TreeNode(nodes[i])
+            q.append(node.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            node.right = TreeNode(nodes[i])
+            q.append(node.right)
+        i += 1
+    return root
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # 1) Recursion -> 느림
+    # def rob(self, root: TreeNode) -> int:
+    #     if not root:
+    #         return 0
+    #     res = root.val
+    #     if root.left:
+    #         res += self.rob(root.left.left) + self.rob(root.left.right)
+    #     if root.right:
+    #         res += self.rob(root.right.left) + self.rob(root.right.right)
+        
+    #     res = max(res, self.rob(root.left) + self.rob(root.right))
+        
+    #     return res
+    # 2) Dynamic Programming (Optimal)
+
+    def rob(self, root: TreeNode) -> int:
+        # DFS를 통해 각 노드에서 두 가지 경우의 최대 이익을 계산:
+        # [0] 현재 노드를 도둑질할 경우의 최대 이익
+        # [1] 현재 노드를 도둑질하지 않을 경우의 최대 이익
+        def dfs(root):
+            if not root:
+                # 노드가 없으면 두 경우 모두 이익은 0
+                return [0, 0]
+
+            # 왼쪽, 오른쪽 서브트리의 결과를 재귀적으로 계산
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            # 현재 노드를 도둑질할 경우: 자식 노드는 도둑질 못함
+            withRoot = root.val + leftPair[1] + rightPair[1]
+
+            # 현재 노드를 도둑질하지 않을 경우: 자식 노드는 도둑질 할 수도, 안 할 수도 있음 (최대값 선택)
+            withoutRoot = max(leftPair) + max(rightPair)
+
+            # 현재 노드에서 [도둑질하는 경우, 도둑질하지 않는 경우] 리턴
+            return [withRoot, withoutRoot]
+
+        # 루트 노드에서 도둑질 하거나 하지 않았을 때의 최대 이익 중 더 큰 값 리턴
+        return max(dfs(root))
+
 if __name__=="__main__":
     sol = Solution()
-    
+
+    root1 = build_tree([1,4,None,2,3,3])
+    print(sol.rob(root1))
--- python/week3/2/[110]Serialize and Deserialize Binary Tree.py
+++ python/week3/2/[110]Serialize and Deserialize Binary Tree.py
@@ -21,6 +21,113 @@ Input: root = []
 
 Output: []
 '''
-if __name__=="__main__":
-    sol = Solution()
+class Queue:
+    def __init__(self):
+        self.data = []
+
+    def append(self, val):
+        self.data.append(val)
+
+    def popleft(self):
+        if self.data:
+            return self.data.pop(0)
+        raise IndexError("pop from empty queue")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __bool__(self):
+        return bool(self.data)
+
+# 입력 리스트를 TreeNode로 변환하는 유틸리티 함수
+def build_tree(nodes):
+    if not nodes or nodes[0] is None:
+        return None
+    root = TreeNode(nodes[0])
+    q = Queue()
+    q.append(root)
+    i = 1
+    while i < len(nodes):
+        node = q.popleft()
+        if i < len(nodes) and nodes[i] is not None:
+            node.left = TreeNode(nodes[i])
+            q.append(node.left)
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            node.right = TreeNode(nodes[i])
+            q.append(node.right)
+        i += 1
+    return root
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Codec 클래스: 이진 트리의 직렬화/역직렬화를 담당
+class Codec:
+    """
+    Codec 클래스는 이진 트리를 문자열로 직렬화하고,
+    문자열로부터 원래의 트리 구조를 복원하는 기능을 제공한다.
+
+    직렬화 방식: DFS 전위 순회 (preorder traversal)
+    - 노드 값은 쉼표로 구분된 문자열로 저장
+    - None(null) 노드는 "N"으로 표시
+    """
+
+    def serialize(self, root: TreeNode) -> str:
+        """
+        이진 트리를 문자열로 직렬화한다.
+        - 전위 순회를 사용하여 왼쪽 → 오른쪽 순서로 탐색
+        - 노드가 없으면 "N"을 기록한다.
+        """
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))  # 현재 노드 값 기록
+            dfs(node.left)             # 왼쪽 서브트리 탐색
+            dfs(node.right)            # 오른쪽 서브트리 탐색
+
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data: str) -> TreeNode:
+        """
+        직렬화된 문자열을 기반으로 이진 트리를 복원한다.
+        - 문자열을 쉼표로 나누어 리스트로 만든 후
+        - 전위 순회 순서대로 재귀적으로 트리를 구성
+        """
+        vals = data.split(",")
+        self.i = 0  # 현재 탐색 중인 인덱스
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))  # 노드 생성
+            self.i += 1
+            node.left = dfs()   # 왼쪽 자식 설정
+            node.right = dfs()  # 오른쪽 자식 설정
+            return node
+
+        return dfs()
+
+# 실행 코드
+if __name__ == "__main__":
+    # 예제 트리: [1,2,3,None,None,4,5]
+    input_list = [1, 2, 3, None, None, 4, 5]
+    root = build_tree(input_list)
+
+    codec = Codec()
+    serialized = codec.serialize(root)
+    print("Serialized:", serialized)
+
+    deserialized_root = codec.deserialize(serialized)
+    re_serialized = codec.serialize(deserialized_root)
+    print("Re-Serialized:", re_serialized)  # 원본과 같아야 함
     
